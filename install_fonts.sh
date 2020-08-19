197ef8d6615635610f7963689a449053b3272d6b
Jackson <jrmoffet@gmail.com>
2020-08-19 13:47:53 -0600

    Add alacritty config, add font install, use functions in setup.sh

---
+++ install_fonts.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+ttyp0_tarball_url='https://people.mpi-inf.mpg.de/~uwe/misc/uw-ttyp0/uw-ttyp0-1.3.tar.gz'
+ttyp0_tarball_name='ttyp0.tar.gz'
+ttyp0_folder=`uw-ttyp0-1.3`
+
+prep_ttyp0 () {
+  wget "${ttyp0_tarball_url}" -O "${ttyp0_tarball_name}"
+  tar -xzf "${ttyp0_tarball_name}"
+  pushd "${ttyp0_folder}"
+  echo 'COPYTO Digit0Slashed Digit0' >> VARIANTS.dat
+  echo 'COPYTO MTilde AccTildeAscii' >> VARIANTS.dat
+  echo 'COPYTO SupAsterisk SAsterisk' >> VARIANTS.dat
+  popd
+}
+
+build_ttyp0 () {
+  pushd "${ttyp0_folder}"
+  ./configure -prefix=/usr/local/share
+  make -j8
+  make install
+  popd
+}
+
+cleanup_ttyp0 () {
+  rm -rf "${ttyp0_folder} ${ttyp0_tarball_name}"
+}
+
+ttyp0 () {
+  prep_ttyp0
+  build_ttyp0
+  cleanup_ttyp0
+}
+
+material_icons () {
+  git clone https://github.com/google/material-design-icons.git
+  pushd material-design-icons
+  cp iconfont/MaterialIcons-Regular.ttf ~/.local/share/fonts
+  popd
+  rm material-design-icons -rf
+}
+
+pushd ~/Downloads
+ttyp0
+material_icons
+popd
+fc-cache -fv
--- setup.sh
+++ setup.sh
@@ -1,140 +1,172 @@
 #!/bin/sh
-if [ -z "$SUDO_USER" ]; then
+if [ ! -z "$SUDO_USER" ]; then
   echo "Make sure to pass the -E argument to sudo"
   exit 1
 fi
-if [ ! -d ~/.config ]; then
-  mkdir ~/.config
-fi
 
-if [ ! -d ~/bin ]; then
-  mkdir ~/bin
-fi
+shell_setup () {
+  echo "Setting shell to zsh"
+  chsh --shell $(which zsh) $SUDO_USER
 
-echo "Copying config files"
-cp -r ./config/* ~/.config
-cp -r ./bin/* ~/bin
-cp -r ./home/* ~
+  echo "Installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
+}
 
+config () {
+  if [ ! -d ~/.config ]; then
+    mkdir ~/.config
+  fi
+
+  if [ ! -d ~/bin ]; then
+    mkdir ~/bin
+  fi
+
+  echo "Copying config files"
+  cp -r ./config/* ~/.config
+  cp -r ./bin/* ~/bin
+  cp -r ./home/* ~
+
+
+  pushd config/i3/backlight_ctrl
+  echo "Building backlight_ctrl"
+  make
+  chown root:root backlight_ctrl
+  chmod u+s backlight_ctrl
+  mv backlight_ctrl ~/bin
+  popd
+
+  source ~/.pathmod.sh
+}
+
+updates () {
 echo "Updating base"
-apt update && apt upgrade -y -qq
-echo "Installing required libs and programs"
-apt install -y git build-essential autotools zsh feh openjdk-8-jdk-headless gradle \
-  curl libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev \
-  libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev \
-  libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev \
-  libtool automake clang cmake cmake-data pkg-config python3 python3-dev python3-sphinx \
-  libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev \
-  python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev \
-  libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libcurl4-openssl-dev \
-  libnl-genl-3-dev pkg-config rofi ninja-build neovim gdb python3-pip libfreetype6-dev \
-  libfontconfig1-dev xclip i3 libxcb-xfixes0-dev network-manager firefox wget htop wireshark thunar -y -qq
+  apt update && apt upgrade -y -qq
 
-pushd config/i3/backlight_ctrl
-echo "Building backlight_ctrl"
-make
-chown root:root backlight_ctrl
-chmod u+s backlight_ctrl
-mv backlight_ctrl ~/bin
+  echo "Installing libraries and programs"
+  apt install -y git build-essential autotools zsh feh openjdk-8-jdk-headless gradle \
+    curl libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev \
+    libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev \
+    libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev \
+    libtool automake clang cmake cmake-data pkg-config python3 python3-dev python3-sphinx \
+    libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev \
+    python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev \
+    libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libcurl4-openssl-dev \
+    libnl-genl-3-dev pkg-config rofi ninja-build neovim gdb python3-pip libfreetype6-dev \
+    libfontconfig1-dev xclip i3 libxcb-xfixes0-dev network-manager firefox wget htop wireshark \
+    thunar openssh-server golang neofetch -y -qq
+}
+
+gui_installs () {
+  echo "Installing Spotify"
+  snap install spotify
+
+  echo "Installing Slack"
+  snap install slack --classic
+
+  if [ ! -d Downloads ]; then
+    mkdir Downloads
+  fi
+  pushd Downloads
+
+  echo "Installing Discord"
+  wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+  dpkg -i discord.deb
+  rm -rf discord.deb
+
+  popd
+}
+
+rust_install () {
+  echo "Installing Rust"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+
+  echo "Installing bat, fd-find, ripgrep"
+  cargo install bat fd-find ripgrep
+}
+
+source_build () {
+  if [ ! -d git ]; then
+    mkdir git
+  fi
+
+  pushd git
+
+  echo "Building i3-gaps"
+  git clone https://github.com/Airblader/i3.git && pushd i3
+  git checkout 4.18.2
+  git submodule update --init
+  autoreconf --force --install
+  mkdir build && pushd build
+  ..configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+  make -j $(nproc)
+  make install
+  popd && popd
+
+  echo "Building Polybar"
+  git clone https://github.com/polybar/polybar.git --recursive
+  pushd polybar
+  mkdir build && pushd build
+  cmake -GNinja
+  ninja
+  ninja install
+  popd && popd
+
+  echo "Building alacritty"
+  git clone https://github.com/alacritty/alacritty.git
+  pushd alacritty
+  git checkout v0.5.0
+  cargo build --release
+  cp target/release/alacritty /usr/local/bin
+  cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+  desktop-file-install extra/linux/Alacritty.desktop
+  update-desktop-database
+  popd
+
+  echo "Removing files"
+  rm -rf i3 polybar alacritty
+
+  popd
+}
+
+vim_setup () {
+  echo "Installing vundle"
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  echo "Installing vim plugins"
+  vim +PluginInstall +qall
+
+  echo "Installing You-Complete-Me"
+  pushd ~/.vim/bundle/YouCompleteMe
+  python3 install.py --clangd-completer --go-completer --rust-completer --java-completer
+  popd
+}
+
+random_tools () {
+  echo "Installing python stuff"
+  pip3 install numpy thefuck i3ipc python-mpd2 dbus-python ipython pwntools
+
+  echo "Installing fzf"
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+}
+
+
+pushd ~
+
+updates
+config
+shell_setup
+gui_installs
+rust_install
+source_build
+random_tools
+vim_setup
 popd
 
-cd ~
-
-echo "Setting shell to zh"
-chsh --shell $(which zsh) $SUDO_USER
-
-echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
-
-echo "Installing Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-
-echo "Installing Spotify"
-snap install spotify
-echo "Installing Slack"
-snap install slack --classic
-
-if [ ! -d Downloads ]; then
-  mkdir Downloads
-fi
-pushd Downloads
-
-echo "Installing Discord"
-wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-dpkg -i discord.deb
-rm -rf discord.deb
-
-echo "Installing Go"
-wget "https://golang.org/dl/go1.15.linux-amd64.tar.gz"
-tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
-rm go1.15.linux-amd64.tar.gz
-
-popd
-
-source ~/.pathmod.sh
-
-if [ ! -d git ]; then
-  mkdir git
-fi
-
-pushd git
-
-echo "Building i3-gaps"
-git clone https://github.com/Airblader/i3.git && pushd i3
-git checkout 4.18.2
-git submodule update --init
-autoreconf --force --install
-mkdir build && pushd build
-..configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-make -j $(nproc)
-make install
-popd && popd
-
-echo "Building Polybar"
-git clone https://github.com/polybar/polybar.git --recursive
-pushd polybar
-mkdir build && pushd build
-cmake -GNinja
-ninja
-ninja install
-popd && popd
-
-echo "Building alacritty"
-git clone https://github.com/alacritty/alacritty.git
-pushd alacritty
-git checkout v0.5.0
-cargo build --release
-cp target/release/alacritty /usr/local/bin
-cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-desktop-file-install extra/linux/Alacritty.desktop
-update-desktop-database
-popd
-
-echo "Removing files"
-rm -rf i3 polybar alacritty
-
-popd
-
-echo "Installing bat, fd-find, ripgrep"
-cargo install bat fd-find ripgrep
-
-echo "Installing python stuff"
-pip3 install numpy thefuck i3ipc python-mpd2 dbus-python ipython
-
-echo "Installing fzf"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-
-echo "Installing vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "Installing vim plugins"
-vim +PluginInstall +qall
-
-echo "Installing you-Complete-Me"
-pushd ~/.vim/bundle/YouCompleteMe
-python3 install.py --clangd-completer --go-completer --rust-completer --java-completer
-popd
+echo "Installing fonts"
+sh install_fonts.sh
 
 echo "Done"
+exit 0
+
+
